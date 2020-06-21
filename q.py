b596a0d4a997caefa7e4f4c82134ce40c6122859
Aidana Bekzhan <aidenelffy@mail.ru>
2020-06-22 01:55:53 +0600

    first commit

---
+++ q.py
@@ -0,0 +1,16 @@
+def div(n):
+    global sum
+    sum = 0
+    if n == 0:
+        return n
+    else:
+        return n % 10 + div(n//10)
+n = int(input())
+m = n % 10
+if div(n) % m == 0:
+    print("YES")
+else: print("NO")
+
+
+
+
